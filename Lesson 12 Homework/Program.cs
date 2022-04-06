0b4718fe5b022f8209a3d4d2682d9d90e0078570
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-04-06 18:45:12 +0300

    Add project files.

---
+++ Lesson 12 Homework/Program.cs
@@ -0,0 +1,44 @@
+﻿using MyClassLib;
+
+var NewsAgency = new NewsProvider();
+
+NewsAgency.AddNews(new[]{
+    new News(NewsCategories.Accidents, "Accident #1"),
+    new News(NewsCategories.Accidents, "Accident #2"),
+    new News(NewsCategories.Accidents, "Accident #3"),
+    new News(NewsCategories.Sports, "Sport #1"),
+    new News(NewsCategories.Sports, "Sport #2"),
+    new News(NewsCategories.Sports, "Sport #3"),
+    new News(NewsCategories.Weather, "Weather #1"),
+    new News(NewsCategories.Weather, "Weather #2"),
+    new News(NewsCategories.Weather, "Weather #3"),
+    new News(NewsCategories.Humour, "Humour #1"),
+    new News(NewsCategories.Humour, "Humour #2"),
+    new News(NewsCategories.Humour, "Humour #3"),
+    new News(NewsCategories.News, "News #1"),
+    new News(NewsCategories.News, "News #2"),
+    new News(NewsCategories.News, "News #3"),
+});
+
+var client1 = new Client();
+var client2 = new Client();
+var client3 = new Client();
+var client4 = new Client();
+var client5 = new Client();
+var client6 = new Client();
+
+client1.AddCategory(new[] { NewsCategories.News, NewsCategories.Humour });
+client2.AddCategory(new[] { NewsCategories.Sports, NewsCategories.Weather });
+client3.AddCategory(new[] { NewsCategories.Accidents, NewsCategories.Humour });
+client4.AddCategory(new[] { NewsCategories.Sports, NewsCategories.Accidents });
+client5.AddCategory(new[] { NewsCategories.News, NewsCategories.Weather });
+client6.AddCategory(new[] { NewsCategories.Humour, NewsCategories.Weather });
+
+NewsAgency.AddConsumer(client1);
+NewsAgency.AddConsumer(client2);
+NewsAgency.AddConsumer(client3);
+NewsAgency.AddConsumer(client4);
+NewsAgency.AddConsumer(client5);
+NewsAgency.AddConsumer(client6);
+
+NewsAgency.SendNews();
---
+++ MyClassLib/MyClassLib.cs
@@ -0,0 +1,145 @@
+﻿namespace MyClassLib
+{
+    public class NewsProvider
+    {
+        List<News> News = new();
+
+        public delegate void SendNewsHandler(string message);
+
+        event SendNewsHandler? NotifyNewsConsumers;
+        event SendNewsHandler? NotifyWeatherConsumers;
+        event SendNewsHandler? NotifySportsConsumers;
+        event SendNewsHandler? NotifyAccidentsConsumers;
+        event SendNewsHandler? NotifyHumourConsumers;
+
+        public void AddNews(NewsCategories category, string message)
+        {
+            News.Add(new News(category, message));
+        }
+
+        public void AddNews(News[] news)
+        {
+            foreach (var item in news)
+            {
+                News.Add(new News(item.Category, item.Message));
+            }
+        }
+
+        public void AddConsumer(Client client)
+        {
+            var clientsCategories = client.GetCategoriesList();
+            clientsCategories.ForEach(category =>
+            {
+                switch (category)   //так и не понял почему тут GetCategoryDelegate(category) не работает как надо
+                {
+                    case NewsCategories.News:
+                        NotifyNewsConsumers += client.Subscribe;
+                        break;
+                    case NewsCategories.Weather:
+                        NotifyWeatherConsumers += client.Subscribe;
+                        break;
+                    case NewsCategories.Sports:
+                        NotifySportsConsumers += client.Subscribe;
+                        break;
+                    case NewsCategories.Accidents:
+                        NotifyAccidentsConsumers += client.Subscribe;
+                        break;
+                    case NewsCategories.Humour:
+                        NotifyHumourConsumers += client.Subscribe;
+                        break;
+                    default:
+                        throw new Exception("Category Not Found!");
+                }
+            });
+        }
+
+        public void SendNews()  //тем не менее тут работает отлично
+        {
+            News.ForEach(news =>
+            {
+                var CategoryDelegate = GetCategoryDelegate(news.Category);
+                CategoryDelegate?.Invoke(news.Message);
+            });
+        }
+
+        private SendNewsHandler? GetCategoryDelegate(NewsCategories category)
+        {
+            switch (category)
+            {
+                case NewsCategories.News:
+                    return NotifyNewsConsumers;
+                case NewsCategories.Weather:
+                    return NotifyWeatherConsumers;
+                case NewsCategories.Sports:
+                    return NotifySportsConsumers;
+                case NewsCategories.Accidents:
+                    return NotifyAccidentsConsumers;
+                case NewsCategories.Humour:
+                    return NotifyHumourConsumers;
+                default:
+                    throw new Exception("Category Not Found!");
+            }
+        }
+    }
+
+    public class Client
+    {
+        List<NewsCategories> Categories = new();
+        public void AddCategory(NewsCategories category)
+        {
+            if (!Categories.Contains(category))
+            {
+                Categories.Add(category);
+            }
+        }
+
+        public void AddCategory(NewsCategories[] categories)
+        {
+            foreach (var category in categories)
+            {
+                if (!Categories.Contains(category))
+                {
+                    Categories.Add(category);
+                }
+            }
+        }
+
+        public void RemoveCategory(NewsCategories category)
+        {
+            if (Categories.Contains(category))
+            {
+                Categories.Remove(category);
+            }
+        }
+
+        public List<NewsCategories> GetCategoriesList()
+        {
+            return Categories;
+        }
+
+        public void Subscribe(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public enum NewsCategories
+    {
+        News,
+        Weather,
+        Sports,
+        Accidents,
+        Humour,
+    }
+
+    public class News
+    {
+        public NewsCategories Category;
+        public string Message = string.Empty;
+        public News(NewsCategories category, string message)
+        {
+            Category = category;
+            Message = message;
+        }
+    }
+}
